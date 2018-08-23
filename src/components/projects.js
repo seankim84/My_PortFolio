0729a7a141a50098cc1e4b76ff01df4a4e01aba0
SeanKim <sean.kim84@gmail.com>
2018-08-23 13:41:25 +0700

    Creating the Tabs

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
+
 
 class Projects extends Component {
-    render(){
+    constructor(props) {
+        super(props)
+        this.state = { activeTab: 2 };
+    }
+
+    render() {
         return (
-            <div>
-                <h1>Projects Page</h1>
-            </div>
-        )
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>React-Native</Tab>
+                    <Tab>GraphQL</Tab>
+                    <Tab>Django</Tab>
+                </Tabs>
+            </div>    
+        );
     }
 }
-
 export default Projects;

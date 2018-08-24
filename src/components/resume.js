8f3ed714d35be33f2cdffd94297d8c36ac64a08b
SeanKim <sean.kim84@gmail.com>
2018-08-24 22:16:42 +0700

    Creating the Resume Page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Resume extends Component {
     render(){
         return (
             <div>
-                <h1>Resume Page</h1>
+                <Grid>
+                    <Cell col={4}>
+                        <div style={{textAlign:'center'}}>
+                            <img src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                                 alt="avatar"
+                                 style={{height: '200px'}}
+                            />
+                        </div>
+
+                        <h2 style={{paddingTop:'2rem'}}>Kim Hyeon Gyu</h2>
+                    </Cell>
+                </Grid>
             </div>
         )
     }

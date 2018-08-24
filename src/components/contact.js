e5f3f101dbe8806e3ce66e9e5f9bbb135e4d7985
SeanKim <sean.kim84@gmail.com>
2018-08-24 10:18:52 +0700

    Create the Contact Page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
     render(){
         return (
-            <div>
-                <h1>Contact Page</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Kim Hyeon Gyu</h2>
+                        <img src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                             alt="avartar"
+                             style={{height: '250px'}}
+                        />
+                        <p style={{width:"75%", margin: "auto", paddingTop: '1em'}}>Full stack developer</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Conatact Me</h2>
+                        <hr />
+                        <div className="contact-list">
+                        <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize:"25px", fontFamily: 'Anton'}}>
+                                    <i className='fa fa-phone-square' aria-hidden="true" />
+                                    +84 90 270 4417
+                                </ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{fontSize:"25px", fontFamily: 'Anton'}}>
+                                    <i class="fa fa-skype" aria-hidden="true" />
+                                    live:sean.kim84
+                                </ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{fontSize:"25px", fontFamily: 'Anton'}}>
+                                    <i class="fa fa-envelope" aria-hidden="true"/>
+                                    sean.kim84@gmail.com
+                                </ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{fontSize:"25px", fontFamily: 'Anton'}}>
+                                    <i class="fa fa-globe"></i>
+                                    Dist.7 Ho Chi Minh City in VietNam
+                                </ListItemContent>
+                            </ListItem>
+                        </List>
+                        </div>
+                    </Cell>
+                    <Cell col={6}></Cell>
+                </Grid>
             </div>
         )
     }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 
 class Projects extends Component {
@@ -8,15 +8,90 @@ class Projects extends Component {
         this.state = { activeTab: 2 };
     }
 
+    toggleCategories() {
+        if(this.state.activeTab === 0){
+            return(
+                <div className="projects-grid">
+                    <Card shadow={0} style={{width: '450px', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff',height:'176px', background: 'url(https://reactjs.org/logo-og.png) center / cover '}}>Welcome</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    <Card shadow={0} style={{width: '450px', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff',height:'176px', background: 'url(https://reactjs.org/logo-og.png) center / cover '}}>Welcome</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    <Card shadow={0} style={{width: '450px', margin: 'auto'}}>
+                        <CardTitle style={{color: '#fff',height:'176px', background: 'url(https://reactjs.org/logo-og.png) center / cover '}}>Welcome</CardTitle>
+                    <CardText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>GitHub</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+                
+                )
+            } else if(this.state.activeTab === 1){
+                return(
+                    <div><h1>React Context API</h1></div>
+                )
+            } else if(this.state.activeTab === 2){
+                return(
+                    <div><h1>React-Native</h1></div>
+                )
+            } else if(this.state.activeTab === 3){
+                return(
+                    <div><h1>GraphQL</h1></div>
+                ) 
+            } else if(this.state.activeTab === 4){
+                    return(
+                        <div><h1>Django</h1></div>
+                    )
+                }
+            }
+
     render() {
         return (
             <div className="category-tabs">
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
                     <Tab>React</Tab>
+                    <Tab>React Context API</Tab>
                     <Tab>React-Native</Tab>
                     <Tab>GraphQL</Tab>
                     <Tab>Django</Tab>
                 </Tabs>
+                <section className="project-grid">
+                    {this.toggleCategories()}
+                </section>
             </div>    
         );
     }

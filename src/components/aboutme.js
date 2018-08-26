c8b094b160b533168e885aba8ee137f1a3f2f69a
SeanKim <sean.kim84@gmail.com>
2018-08-26 21:55:47 +0700

    Creating the Introduce

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import { Grid, Cell} from 'react-mdl';
+import Introduce from './introduce';
 
 class AboutMe extends Component {
     render(){
         return (
             <div>
-                <h1>About Page</h1>
+                <Grid>
+                    <Cell col={4}>
+                        <div style={{textAlign:'center'}}>
+                            <img src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                                 alt="avatar"
+                                 style={{height: '200px'}}
+                            />
+                        </div>
+
+                        <h2 style={{paddingTop:'2rem'}}>Kim Hyeon Gyu</h2>
+                        <h4 style={{color: 'grey'}}>Coder</h4>
+                        <hr style={{borderTop: '3px solid #833fb2', width:'50%'}} />
+                        <p>Programming is really fun</p>
+                        <hr style={{borderTop:'3px solid #833fb2', width:'50%'}}/>
+                        <h5>Address</h5>
+                        <p>Dist.7 Ho Chi Minh City, Vietnam</p>
+                        <h5>Phone</h5>
+                        <p>+84 90 270 4417</p>
+                        <h5>Email</h5>
+                        <p>sean.kim84@gmail.com</p>
+                        <h5>Web</h5>
+                        <p><a href="https://seankim84.github.io/PortFolio/Profile.html" target="blank">https://seankim84.github.io/PortFolio/Profile.html</a></p>
+                        <hr style={{borderTop: '3px solid #833fb2', width:'50%'}} />
+                        </Cell>
+                        <Cell className="resume-right-col" col={8}>
+                            <h2>Introduce</h2>
+                            <Introduce 
+                                startYear={2014} 
+                                endYear={2016}
+                                schoolName="KANG WON National Univ"
+                                schoolDescription="InterNational Univ"
+                            />
+                        </Cell>
+                </Grid>
             </div>
         )
     }
---
+++ src/components/introduce.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Introduce extends Component {
+    render() {
+        return(
+            <div>
+                <Grid>
+                    <Cell>
+                    
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Introduce
